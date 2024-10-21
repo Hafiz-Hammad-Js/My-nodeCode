e26792f2dd13cd954fa3f80076bb7d0f9e141d11
hafiz-hammad-js <muhaamadhammad7860@gmail.com>
2024-10-21 17:43:21 +0500

    cahnges for  host

--- index.mjs
+++ index.mjs
@@ -12,6 +12,10 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use('/', Router)
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const PORT =process.env.PORT || 3000
 
 app.listen(PORT, () => {
